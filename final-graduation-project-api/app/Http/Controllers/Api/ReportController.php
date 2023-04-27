e1ac761ba73f55806a4adc55ecccd65ae1c486be
Yuri Miranda Fialho <mirandafialho@gmail.com>
2023-04-27 01:27:09 -0300

    Coding controllers for tickets and reports

--- final-graduation-project-api/app/Http/Controllers/Api/ReportController.php
+++ final-graduation-project-api/app/Http/Controllers/Api/ReportController.php
@@ -4,62 +4,58 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Report;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Report::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $report = Report::query()->create($request->all());
+
+        return response()->json($report);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Report $report)
+    public function show(int $id): JsonResponse
     {
-        //
+        try {
+            $report = Report::query()->findOrFail($id);
+
+            return response()->json($report);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Report $report)
+    public function update(Request $request, int $id): JsonResponse
     {
-        //
+        try {
+            $report = Report::query()->findOrFail($id);
+
+            $report->update($request->all());
+
+            return response()->json($report);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Report  $report
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Report $report)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        try {
+            $report = Report::query()->findOrFail($id);
+
+            $report->delete();
+
+            return response()->json(['status' => true, 'message' => 'deleted']);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 }
--- final-graduation-project-api/app/Http/Controllers/Api/TicketController.php
+++ final-graduation-project-api/app/Http/Controllers/Api/TicketController.php
@@ -4,62 +4,58 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Ticket::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $ticket = Ticket::query()->create($request->all());
+
+        return response()->json($ticket);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Ticket  $ticket
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Ticket $ticket)
+    public function show(int $id): JsonResponse
     {
-        //
+        try {
+            $ticket = Ticket::query()->findOrFail($id);
+
+            return response()->json($ticket);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Ticket  $ticket
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Ticket $ticket)
+    public function update(Request $request, int $id): JsonResponse
     {
-        //
+        try {
+            $ticket = Ticket::query()->findOrFail($id);
+
+            $ticket->update($request->all());
+
+            return response()->json($ticket);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Ticket  $ticket
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Ticket $ticket)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        try {
+            $ticket = Ticket::query()->findOrFail($id);
+
+            $ticket->delete();
+
+            return response()->json(['status' => true, 'message' => 'deleted']);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => $exception->getMessage()], 404);
+        }
     }
 }
